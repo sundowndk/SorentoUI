f9419745582f7f3110d282a28b711240b690da84
Rasmus Pedersen <rasmus@akvaservice.dk>
2011-10-12 16:06:26 +0200

    -

--- Source/web/console.jsb
+++ Source/web/console.jsb
@@ -1,10 +1,12 @@
 <solution name="Console" outputdirectory="">	
 	<project name="console">
-		<class name="console">	
-			<class name="modal">
-				<js file="javascript/modal/init.js" />
-				<js file="javascript/modal/window.js" />
-				<js file="javascript/modal/question.js" />
+		<class name="sorento">
+			<class name="console">				
+				<class name="modal">
+					<js file="javascript/modal/init.js" />
+					<js file="javascript/modal/window.js" />
+					<js file="javascript/modal/question.js" />
+				</class>				
 			</class>
 		</class>
 	</project>	
--- Source/web/javascript/modal/question.js
+++ Source/web/javascript/modal/question.js
@@ -1,79 +1,41 @@
 question : function (attributes)
 {
-	var modal = new sorentoUI.modalWindow (attributes);
+	var modal = new console.modal.window (attributes);
 	
 	var action =	function (result)
-			{
-				attributes.onDone (result);
-				modal.dispose ();
-			};
-	
-	var container = new SNDK.SUI.container ({appendTo: modal.getContentElement (), width: "300px", height: "150px", title: "Question ?"})
-	var layoutbox = new SNDK.SUI.layoutbox ({appendTo: container.content (), type: "horizontal", width: "100%", height: "100%"});
-	layoutbox.addPanel ({tag: "panel1", size: "*"});
-	layoutbox.addPanel ({tag: "panel2", size: "45px"});
-	
-	var label =  new SNDK.SUI.label ({appendTo: layoutbox.getPanel ("panel1"), text: attributes.text, width: "100%"});
-	var button1 = new SNDK.SUI.button ({appendTo: layoutbox.getPanel ("panel2"), width: "100px", label: attributes.buttonLabel.split ("|")[0], onClick: function () {action (1)}});
-	var button2 = new SNDK.SUI.button ({appendTo: layoutbox.getPanel ("panel2"), width: "100px", label: attributes.buttonLabel.split ("|")[1], onClick: function () {action (2)}});
-	
+					{
+						modal.dispose ();
+						attributes.onDone (result);
+						
+					};
+
+	var canvas = new SNDK.SUI.canvas ({appendTo: modal.getContentElement (), width: "600px", height: "200px"});
+			
+	var container = new SNDK.SUI.container ({title: "Question ?"});
+			
+	var layoutbox1 = new SNDK.SUI.layoutbox ({type: "horizontal"});
+	layoutbox1.addPanel ({tag: "panel1", size: "*"});
+	layoutbox1.addPanel ({tag: "panel2", size: "55px"});
+
+	var layoutbox2 = new SNDK.SUI.layoutbox ({type: "vertical"});
+	layoutbox2.addPanel ({tag: "panel1", size: "*"});
+	layoutbox2.addPanel ({tag: "panel2", size: "210px"});
+												
+	var label = new SNDK.SUI.label ({text: attributes.text});
+			
+	var button1 = new SNDK.SUI.button ({label: attributes.buttonLabel.split ("|")[0], width: "100px", onClick: function () {action (1)}});
+	var button2 = new SNDK.SUI.button ({label: attributes.buttonLabel.split ("|")[1], width: "100px", onClick: function () {action (2)}});			
+															
+	canvas.addUIElement (container);
+	container.addUIElement (layoutbox1);
+			
+	layoutbox1.getPanel ("panel1").addUIElement (label);
+	layoutbox1.getPanel ("panel2").addUIElement (layoutbox2);			
+									
+	layoutbox2.getPanel ("panel2").addUIElement (button1);
+	layoutbox2.getPanel ("panel2").addUIElement (button2);
+			
 	SNDK.SUI.init ();
 	
 	modal.show ();
-					
-	
-//				if (options.buttonText == null)
-//				{
-//					throw "modalQuestion - buttonText not defined.";
-//				}
-				
-//				var button1text = options.buttonText.split ("|")[0];
-//				var button2text = options.buttonText.split ("|")[1];
-		
-//				if (button1text == "" || button1text == null)
-//				{
-//					buttontext1 = "_YES_";
-//				}
-		
-//				if (button2text == "" || button2text == null)
-//				{
-//					buttontext2 = "_NO_";
-//				}				
-
-//				// Create MODALWINDOW
-//				var modalwindow = new sorentoUI.modalWindow ("Question", options.title);
-											
-//				// Create MODALWINDOW content.
-//				var elements = new Array ();
-		
-//				// DESCRIPTION
-//				modalwindow.addDescription ({text: options.text});												
-
-//				// BUTTON1								
-//				var button1onclick = 	function () 
-//							{											
-//								modalwindow.dispose ();
-//
-//								if (options.onDone != null)									
-//								{
-//									setTimeout (function () {options.onDone (1);}, 1);
-//								}
-//							};
-//								
-//				modalwindow.addButton ({label: "Add", label: button1text, onClick: button1onclick});							
-//
-//				// BUTTON2
-//				var button2onclick =	function () 
-//							{											
-//								modalwindow.dispose ();
-//
-//								if (options.onDone != null)									
-//								{
-//									setTimeout (function ()	{options.onDone (2);}, 1);
-//								}
-//							};
-//								
-//				modalwindow.addButton ({label: "Add", label: button2text, onClick: button2onclick});
-//																											
-//				modalwindow.show ();	
 }	
