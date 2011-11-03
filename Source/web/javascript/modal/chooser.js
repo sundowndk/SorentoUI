d9d946f797eb6e1712e254ec4a55fab767498660
Rasmus Pedersen <rasmus@akvaservice.dk>
2011-11-03 15:43:30 +0100

    -

--- Source/web/javascript/modal/chooser.js
+++ Source/web/javascript/modal/chooser.js
@@ -1,50 +1,133 @@
+// -------------------------------------------------------------------------------------------------------------------------
+// chooser ([attributes])
+// -------------------------------------------------------------------------------------------------------------------------
+//
+// .getContentElement ()
+//
+// .getAttribute (string)
+// .setAttribute (string, string)
+//	
+//		title 			get/set
+//		buttonLabel		get/set
+//		onButton1Click	set
+//		onButton2Click	set
+//
+
 chooser : function (attributes)
 {
-	var modal = new sorento.console.modal.window (attributes);
-	
-	var action = function ()
-	{
-		modal.dispose ();	
-	}
-	
-	var canvas = new SNDK.SUI.canvas ({appendTo: modal.getContentElement (), width: "600px", height: "400px"});
-			
-	var container = new SNDK.SUI.container ({title: attributes.title});
-			
-	var layoutbox1 = new SNDK.SUI.layoutbox ({type: "horizontal"});
-	layoutbox1.addPanel ({tag: "panel1", size: "*"});
-	layoutbox1.addPanel ({tag: "panel2", size: "55px"});
+	_attributes = attributes;
+	_temp = { initialized: false,
+			  uiElements: new Array };
+				
+	setAttributes ();
+	construct ();
+	init ();
 
-	var layoutbox2 = new SNDK.SUI.layoutbox ({type: "vertical"});
-	layoutbox2.addPanel ({tag: "panel1", size: "*"});
-	layoutbox2.addPanel ({tag: "panel2", size: "210px"});
-													
-			
-	var button1 = new SNDK.SUI.button ({label: attributes.buttonLabel, width: "100px", onClick: function () {action ()}});
-	var button2 = new SNDK.SUI.button ({label: attributes.buttonLabel, width: "100px", onClick: function () {action ()}});
-															
-	canvas.addUIElement (container);
-	container.addUIElement (layoutbox1);
-			
-	layoutbox1.getPanel ("panel2").addUIElement (layoutbox2);			
+	this.show = functionShow;
+	this.hide = functionHide;
+	this.dispose = functionDispose;
+	
+	this.getContentElement = functionGetContentElement;
+	
+	this.setAttribute = functionSetAttribute;
+	this.getAttribute = functionGetAttribute;	
+
+	// ------------------------------------
+	// init
+	// ------------------------------------		
+	function init ()
+	{				
+	}
+
+	// ------------------------------------
+	// construct
+	// ------------------------------------	
+	function construct ()
+	{
+		_temp.uiElements.modal = new sorento.console.modal.window (_attributes);		
+	
+		_temp.uiElements.canvas = new SNDK.SUI.canvas ({appendTo: _temp.uiElements.modal.getContentElement (), width: "600px", height: "400px"});
+		_temp.uiElements.container = new SNDK.SUI.container ({title: _attributes.title});
+		
+		_temp.uiElements.layoutbox1 = new SNDK.SUI.layoutbox ({type: "horizontal"});
+		_temp.uiElements.layoutbox1.addPanel ({tag: "panel1", size: "*"});
+		_temp.uiElements.layoutbox1.addPanel ({tag: "panel2", size: "55px"});		
+	
+		_temp.uiElements.layoutbox2 = new SNDK.SUI.layoutbox ({type: "vertical"});
+		_temp.uiElements.layoutbox2.addPanel ({tag: "panel1", size: "*"});
+		_temp.uiElements.layoutbox2.addPanel ({tag: "panel2", size: "210px"});	
+		
+		_temp.uiElements.button1 = new SNDK.SUI.button ({label: _attributes.buttonLabel.split ("|")[0], width: "100px", onClick: function () {action ()}});
+		_temp.uiElements.button2 = new SNDK.SUI.button ({label: _attributes.buttonLabel.split ("|")[1], width: "100px", onClick: function () {action ()}});	
+
+		_temp.uiElements.canvas.addUIElement (_temp.uiElements.container);
+		_temp.uiElements.container.addUIElement (_temp.uiElements.layoutbox1);
+	
+		_temp.uiElements.layoutbox1.getPanel ("panel2").addUIElement (_temp.uiElements.layoutbox2);
 									
-	layoutbox2.getPanel ("panel2").addUIElement (button1);
-	layoutbox2.getPanel ("panel2").addUIElement (button2);
+		_temp.uiElements.layoutbox2.getPanel ("panel2").addUIElement (_temp.uiElements.button1);
+		_temp.uiElements.layoutbox2.getPanel ("panel2").addUIElement (_temp.uiElements.button2);	
+	}	
 	
-	var elements = SNDK.SUI.builder.construct ({URL: attributes.url, appendTo: layoutbox1.getPanel ("panel1")});
-	
-	SNDK.SUI.init ();
-	
-	if (attributes.beforeShow != null)
+	// ------------------------------------
+	// setDefaultAttributes
+	// ------------------------------------					
+	function setAttributes ()
 	{
-		attributes.beforeShow (elements);
+		if (!_attributes) _attributes = new Array ();
+		
+		if (!_attributes.title) _attributes.title = "";
+		
+		if (!_attributes.buttonLabel) _attributes.buttonLabel = "|";
+	}		
+	
+	function functionShow ()
+	{
+		_temp.uiElements.modal.show ();
 	}
 	
+	function functionHide ()
+	{
+		_temp.uiElements.modal.hide ();
+	}
 	
-	modal.show ();	
-
+	function functionDispose ()
+	{
+		_temp.uiElements.modal.dispose ();
+	}
 	
-	return elements;
+	function functionGetContentElement ()
+	{
+		return _temp.uiElements.layoutbox1.getPanel ("panel1");
+	}
+	
+	// ------------------------------------
+	// getAttribute
+	// ------------------------------------						
+	function functionGetAttribute (attribute)
+	{
+		switch (attribute)
+		{								
+			default:
+			{
+				throw "No attribute with the name '"+ attribute +"' exist in this object";
+			}
+		}	
+	}
+	
+	// ------------------------------------
+	// setAttribute
+	// ------------------------------------						
+	function functionSetAttribute (attribute, value)
+	{
+		switch (attribute)
+		{
+			default:
+			{
+				throw "No attribute with the name '"+ attribute +"' exist in this object";
+			}
+		}	
+	}										
 }	 
 
 
--- Source/web/javascript/modal/usergroup_chooser.js
+++ Source/web/javascript/modal/usergroup_chooser.js
@@ -1,17 +1,29 @@
-usergroupChooser : function ()
+usergroupChooser : function (attributes)
 {
-	var beforeshow = 	function (elements)
-						{
-							elements.usergroups.setItems (sorento.usergroup.list ());
-						};
+	var chooser = new sorento.console.modal.chooser ({title: "Choose usergroup", buttonLabel: "Ok|Cancel"});
+	
+	var elements = SNDK.SUI.builder.construct ({URL: "/console/xml/chooser/usergroup.xml", appendTo: chooser.getContentElement ()});		
+	elements.usergroups.setItems (sorento.usergroup.list ());
 						
-	var aftershow =		function ()
-						{
+	SNDK.SUI.init ();
+	
+	chooser.show ();
+
+//	var beforeshow = 	function (elements)
+//						{
+//							elements.usergroups.setItems (sorento.usergroup.list ());
+//						};
 						
-						};
+//	var aftershow =		function ()
+//						{
+						
+//						};
 	
 
-	var elements = sorento.console.modal.chooser ({title: "Choose usergroup", url: "/console/xml/chooser/usergroup.xml", beforeShow: beforeshow});
+
+
+
+	//var elements = sorento.console.modal.chooser ({title: "Choose usergroup", url: "/console/xml/chooser/usergroup.xml", beforeShow: beforeshow});
 				
 }	
 
