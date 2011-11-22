cd9e19eed5e3a42d78a5fcfd6f52cf322b692f65
Rasmus Pedersen <rasmus@akvaservice.dk>
2011-11-22 16:14:52 +0100

    -

--- Source/web/javascript/modal/error.js
+++ Source/web/javascript/modal/error.js
@@ -5,29 +5,36 @@ error : function (attributes)
 	var action = function ()
 	{
 		modal.dispose ();
+		
+		if (attributes.onDone != null)
+		{
+			attributes.onDone ();
+		}
+		
+		
 	
 	}
 	
-	var canvas = new SNDK.SUI.canvas ({appendTo: modal.getContentElement (), width: "600px", height: "175px"});
+	var canvas = new SNDK.SUI.canvas ({appendTo: modal.getContentElement (), width: "600px", height: "180px"});
 			
-	var container = new SNDK.SUI.container ({title: "Error!"});
+	var container = new SNDK.SUI.container ({title: attributes.title, stylesheet: "SUIContainerModal"});
 			
-	var layoutbox1 = new SNDK.SUI.layoutbox ({type: "horizontal"});
+	var layoutbox1 = new SNDK.SUI.layoutbox ({type: "horizontal", stylesheet: "SUILayoutBoxNoBorder"});
 	layoutbox1.addPanel ({tag: "panel1", size: "*"});
-	layoutbox1.addPanel ({tag: "panel2", size: "55px"});
+	layoutbox1.addPanel ({tag: "panel2", size: "48px"});
 
 	var layoutbox2 = new SNDK.SUI.layoutbox ({type: "vertical"});
 	layoutbox2.addPanel ({tag: "panel1", size: "*"});
 	layoutbox2.addPanel ({tag: "panel2", size: "105px"});
 												
-	var label = new SNDK.SUI.label ({text: attributes.text});
+	var text = new SNDK.SUI.text ({text: attributes.text});
 			
-	var button1 = new SNDK.SUI.button ({label: attributes.buttonLabel, width: "100px", onClick: function () {action ()}});
+	var button1 = new SNDK.SUI.button ({label: attributes.buttonLabel, width: "100px", onClick: function () {action ()}, focus: true});
 															
 	canvas.addUIElement (container);
 	container.addUIElement (layoutbox1);
 			
-	layoutbox1.getPanel ("panel1").addUIElement (label);
+	layoutbox1.getPanel ("panel1").addUIElement (text);
 	layoutbox1.getPanel ("panel2").addUIElement (layoutbox2);			
 									
 	layoutbox2.getPanel ("panel2").addUIElement (button1);
--- Source/web/javascript/modal/question.js
+++ Source/web/javascript/modal/question.js
@@ -6,30 +6,30 @@ question : function (attributes)
 					{
 						modal.dispose ();
 						attributes.onDone (result);
-						
 					};
 
-	var canvas = new SNDK.SUI.canvas ({appendTo: modal.getContentElement (), width: "600px", height: "200px"});
+	var canvas = new SNDK.SUI.canvas ({appendTo: modal.getContentElement (), width: "600px", height: "180px"});
 			
-	var container = new SNDK.SUI.container ({title: "Question ?"});
+	var container = new SNDK.SUI.container ({title: attributes.title, stylesheet: "SUIContainerModal"});
 			
-	var layoutbox1 = new SNDK.SUI.layoutbox ({type: "horizontal"});
+	var layoutbox1 = new SNDK.SUI.layoutbox ({type: "horizontal", stylesheet: "SUILayoutBoxNoBorder"});
+	layoutbox1.addPanel ({tag: "panel3", size: "15px"});
 	layoutbox1.addPanel ({tag: "panel1", size: "*"});
-	layoutbox1.addPanel ({tag: "panel2", size: "55px"});
+	layoutbox1.addPanel ({tag: "panel2", size: "48px"});
 
-	var layoutbox2 = new SNDK.SUI.layoutbox ({type: "vertical"});
+	var layoutbox2 = new SNDK.SUI.layoutbox ({type: "vertical"});	
 	layoutbox2.addPanel ({tag: "panel1", size: "*"});
 	layoutbox2.addPanel ({tag: "panel2", size: "210px"});
 												
-	var label = new SNDK.SUI.label ({text: attributes.text});
+	var text = new SNDK.SUI.text ({text: attributes.text});
 			
 	var button1 = new SNDK.SUI.button ({label: attributes.buttonLabel.split ("|")[0], width: "100px", onClick: function () {action (1)}});
-	var button2 = new SNDK.SUI.button ({label: attributes.buttonLabel.split ("|")[1], width: "100px", onClick: function () {action (2)}});			
+	var button2 = new SNDK.SUI.button ({label: attributes.buttonLabel.split ("|")[1], width: "100px", onClick: function () {action (2)}, focus: true});			
 															
 	canvas.addUIElement (container);
 	container.addUIElement (layoutbox1);
 			
-	layoutbox1.getPanel ("panel1").addUIElement (label);
+	layoutbox1.getPanel ("panel1").addUIElement (text);
 	layoutbox1.getPanel ("panel2").addUIElement (layoutbox2);			
 									
 	layoutbox2.getPanel ("panel2").addUIElement (button1);
